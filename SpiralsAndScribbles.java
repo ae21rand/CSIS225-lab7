e3111fca4924d9dd08a78956b41ece12241fd6c7
ae21rand <ae21rand@siena.edu>
2026-03-25 14:42:54 -0400

    Implemented all support methods: randomColor(), recolorAll(), randomColor(boolean isCrazy).

--- SpiralsAndScribbles.java
+++ SpiralsAndScribbles.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -16,9 +17,10 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
- * This program draws spirals and scribbles on the screen. 
- * The user can choose to draw spirals or scribbles, and can also choose different color modes.
- * The user can clear the drawings if the mouse exits the drawing area, 
+ * This program draws spirals and scribbles on the screen.
+ * The user can choose to draw spirals or scribbles, and can also choose
+ * different color modes.
+ * The user can clear the drawings if the mouse exits the drawing area,
  * depending on the checkbox selection.
  * 
  * @author Prof. White
@@ -44,13 +46,57 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
     /** The JComboBox for selecting what to draw: spirals or scribbles. */
     private JComboBox drawWhat;
 
-    /**The JCheckBox for clearing the GUI window. */
+    /** The JCheckBox for clearing the GUI window. */
     private JCheckBox clearOnExit;
 
-    /** The JComboBox for selecting the color.  */
+    /** The JComboBox for selecting the color. */
     private JComboBox colorMode;
 
-    /** 
+    protected static Random rand = new Random();
+
+    /**
+     * This method returns a new Color object with randomly selected RGB
+     * values in the range [0, 255] using the class variable.
+     * 
+     * @return a new Color object with randomly selected RGB values in the range [0,
+     *         255]
+     */
+    public static Color randomColor() {
+        return new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+    }
+
+    /**
+     * This method recolors all objects in the items list.
+     */
+    protected void recolorAll() {
+        for (ShapesOfScenes obj : items) {
+            for (ColorLineSegment line : obj.lines) {
+                line.c = randomColor();
+            }
+        }
+    }
+
+    public void randomColor(boolean isCrazy) {
+        Color c;
+        if (isCrazy) {
+            for (ShapesOfScenes obj : items) {
+                for (ColorLineSegment line : obj.lines) {
+                    line.c = randomColor();
+                }
+            }
+        } else {
+            Color newColor = randomColor();
+
+            for (ShapesOfScenes obj : items) {
+                for (ColorLineSegment line : obj.lines) {
+                    line.c = newColor;
+                }
+            }
+            panel.repaint();
+        }
+    }
+
+    /**
      * This method is called by the paintComponent method of
      * the anonymous extension of JPanel, to keep that method
      * from getting too long.
@@ -91,7 +137,7 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
                 redraw(g);
             }
         };
-        
+
         outerPanel.add(panel, BorderLayout.CENTER);
         panel.addMouseListener(this);
         panel.addMouseMotionListener(this);
@@ -107,7 +153,7 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
         clearOnExit.setSelected(false);
         controlPanel.add(clearOnExit);
 
-        String[] colorOptions = {"Black", "Colorful", "More Colorful", "Crazy Colorful"};
+        String[] colorOptions = { "Black", "Colorful", "More Colorful", "Crazy Colorful" };
         colorMode = new JComboBox<>(colorOptions);
         colorMode.setSelectedIndex(0);
         controlPanel.add(colorMode);
@@ -120,8 +166,8 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
     }
 
     /**
-     * This method is called when the mouse is pressed. It initializes a new 
-     * ShapesOfScenes object and sets the current color based on the selected 
+     * This method is called when the mouse is pressed. It initializes a new
+     * ShapesOfScenes object and sets the current color based on the selected
      * color mode.
      * 
      * @param e the MouseEvent containing information about the mouse press
@@ -135,7 +181,7 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
     }
 
     /**
-     * This method is called when the mouse is moved. 
+     * This method is called when the mouse is moved.
      * It repaints the panel to reflect any changes in the drawing.
      * 
      * @param e the MouseEvent containing information about the mouse movement
@@ -146,8 +192,9 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
     }
 
     /**
-     * This method is called when the mouse is dragged. It creates a new ColorLineSegment 
-     * from the press point to the current mouse position, adds it to the current 
+     * This method is called when the mouse is dragged. It creates a new
+     * ColorLineSegment
+     * from the press point to the current mouse position, adds it to the current
      * ShapesOfScenes, and repaints the panel.
      * 
      * @param e the MouseEvent containing information about the mouse drag
@@ -169,7 +216,7 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
     }
 
     /**
-     * This method is called when the mouse exits the drawing area. 
+     * This method is called when the mouse exits the drawing area.
      * If the "Clear on exit" checkbox is selected, it clears all the drawings.
      */
     @Override
@@ -178,10 +225,11 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
             items.clear();
             panel.repaint();
         }
-   }
+    }
 
     /**
-     * This method is called when an action event occurs, such as a button press or menu selection.
+     * This method is called when an action event occurs, such as a button press or
+     * menu selection.
      * In this program, we don't have any buttons, so this method is not used.
      */
     @Override
@@ -190,7 +238,7 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
     }
 
     /**
-     * The main method is responsible for creating a thread 
+     * The main method is responsible for creating a thread
      * that will construct and show the graphical user interface.
      */
     public static void main(String args[]) {
@@ -200,7 +248,8 @@ public class SpiralsAndScribbles extends MouseAdapter implements Runnable, Actio
 
 /**
  * A ColorLineSegment is a line segment with a color.
- * The line segment is defined by two points, and the color is used when drawing the line.
+ * The line segment is defined by two points, and the color is used when drawing
+ * the line.
  */
 class ColorLineSegment {
 
@@ -218,7 +267,8 @@ class ColorLineSegment {
      * Constructs a ColorLineSegment with the specified color and endpoints.
      * 
      * @param c the color of the line segment
-     * @param p an array of two points representing the endpoints of the line segment
+     * @param p an array of two points representing the endpoints of the line
+     *          segment
      */
     public ColorLineSegment(Color c, Point[] p) {
 
@@ -270,7 +320,8 @@ class ShapesOfScenes {
     }
 
     /**
-     * Determines if the given point is near any of the line segments in the collection.
+     * Determines if the given point is near any of the line segments in the
+     * collection.
      * 
      * @param p the point to check
      * @return true if the point is near any line segment, false otherwise
